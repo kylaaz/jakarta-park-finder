ffd2ee70a5928b72c191183c19eaeb53d031499b
Abian Nanda <159929245+abianrndev@users.noreply.github.com>
2024-12-12 00:37:21 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './view/pages/HomePage';
+import ParkForm from './components/park/ParkForm';
+import ParkList from './components/park/ParkList';
+import ParkDetail from './components/park/ParkDetail';
 import Modal from './components/auth/Modal';
 
 function App() {
   return (
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/parks" element={<ParkList />} />
+      <Route path="/parks/new" element={<ParkForm />} />
+      <Route path="/parks/:id" element={<ParkDetail />} />
+    </Routes>
+    
     <>
-      <h1 className="text-3xl font-bold">Jakarta Finder</h1>
-
       <Modal />
     </>
   );
