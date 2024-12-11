2d646d665f434cea2e2d36a115803cc9334fa4af
Abian Nanda <159929245+abianrndev@users.noreply.github.com>
2024-12-11 23:48:14 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api', // Sesuaikan dengan port backend Anda
+    baseURL: 'http://localhost:3000/api',
     headers: {
         'Content-Type': 'application/json',
     },
