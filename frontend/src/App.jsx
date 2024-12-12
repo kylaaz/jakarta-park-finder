a544e67a18bc6b07f54afaf8bbae83e16aad6375
fadlyhts <fadlyhutasuhut1305@gmail.com>
2024-12-12 17:10:11 +0700

    update route configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-
-import ParkDetail from './components/park/ParkDetail';
-import ParkList from './components/park/ParkList';
-import HomePage from './view/pages/HomePage';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './view/pages/HomePage'
+import AboutPage from './view/pages/AboutPage'
+import InformationPage from './view/pages/InformationPage'
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/parks" element={<ParkList />} />
-        <Route path="/parks/:id" element={<ParkDetail />} />
-      </Routes>
-    </>
-  );
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/information" element={<InformationPage />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
