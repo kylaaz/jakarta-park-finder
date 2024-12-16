34598908a66b158e29ca483efb869ef6685be324
Aldian.J <aldijuventiano@student.telkomuniversity.ac.id>
2024-12-17 06:25:15 +0700

    add path notifications page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import AboutPage from './view/pages/AboutPage';
 import HomePage from './view/pages/HomePage';
 import InformationPage from './view/pages/InformationPage';
+import NotificationsPage from './view/pages/NotificationsPage';
 import ParkDetailPage from './view/pages/ParkDetailPage';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="/about" element={<AboutPage />} />
       <Route path="/information" element={<InformationPage />} />
       <Route path="/park/:id" element={<ParkDetailPage />} />
+      <Route path="/notifications" element={<NotificationsPage />} />
     </Routes>
   );
 }
